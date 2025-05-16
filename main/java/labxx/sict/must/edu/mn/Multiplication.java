8f5f47b91849ac04f803540a3fff814e6fa0fb6b
bargan123 <gansukhbarkhas@gmail.comm>
2025-05-16 14:20:45 +0800

    Add conflict version B comment to multiply()

--- main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,5 +1,8 @@
 package labxx.sict.must.edu.mn;
 
+/**
+ * Multiply two double values (conflict version B).
+ */
 public class Multiplication {
     public double multiply(double a, double b) {
         return a * b;
