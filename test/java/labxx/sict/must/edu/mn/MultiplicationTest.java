49a9bd56f22475520dcfd12bf1d8a2e126757c76
bargan123 <gansukhbarkhas@gmail.comm>
2025-05-16 11:32:10 +0800

    Fix: add MultiplicationTest for CI coverage

--- test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
+++ test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
@@ -7,12 +7,12 @@ public class MultiplicationTest {
     @Test
     void testMultiplyPositive() {
         Multiplication calc = new Multiplication();
-        assertEquals(6.0, calc.multiply(2.0, 3.0), "2 * 3 should be 6");
+        assertEquals(6.0, calc.multiply(2.0, 3.0));
     }
 
     @Test
     void testMultiplyNegative() {
         Multiplication calc = new Multiplication();
-        assertEquals(-6.0, calc.multiply(-2.0, 3.0), "-2 * 3 should be -6");
+        assertEquals(-6.0, calc.multiply(-2.0, 3.0));
     }
 }
